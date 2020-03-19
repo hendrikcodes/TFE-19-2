640f4ec6cd9a1d2856e2e9a686b7a0741289fc54
hendrikcodes <62388191+hendrikcodes@users.noreply.github.com>
2020-03-19 20:52:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,126 +1,179 @@
-#include <stdbool.h>
-#include <stdlib.h>
 #include "Header.h"
 #include <stdio.h>
-#include <assert.h>
-#define N 6
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct Queue{
-           int* memory[100];
-           size_t capacity;
-           int i;
-           bool used;
-           int* rear;
-           int* front;
-}Queue;
+#define MAX 6
 
- ueue queue_pool[10];
+struct Queue
+{
+    int* memory;
+    int front;
+    int back;
+    size_t capacity;
+    size_t length;
+    bool used;
+};
+
+Queue* Pool[MAX];                          //creating a pool of queue array with MAX places
 
 Queue* Queue_create(int* memory, size_t capacity)
 {
-        
-           for (int i = 0; i < 10; i++)
-           {
-                     Queue queue = queue_pool[i];
-                     if (queue_pool[i].used == false)
-                     {    
-                     queue.memory = memory;
-                     queue.capacity = capacity;
-                     queue.rear = memory;
-                     queue.front = memory;
-                     queue.used = true;
-                     return &queue;
-                     }
-           }
-           return NULL;
+                       
+    if (memory != NULL)                            //test if memory is not empty and pointing nowhere
+    {
+        for (int i = 0; i <= MAX; i++)
+        {
+            if (*memory = NULL)                    //test if queue is availabe for use
+            {
+                Pool[i]->memory = memory;
+                Pool[i]->capacity = capacity;
+                Pool[i]->back = Pool[i]->front = -1;
+                Pool[i]->length = 0;
+                Pool[i]->used = true;
+                return &Pool[i];
+            }
+        };
+    }
+    else {
+        return NULL;
+    }
 }
 
 void Queue_destroy(Queue* queue)
 {
-           queue->memory = 0;
-           queue->capacity = 0;
-           queue->used = false;
+    if (queue != NULL) {
+        queue->memory = NULL;
+        queue->back = queue->front = -1;
+        queue->capacity = 0;
+        queue->length = 0;
+    }
 }
 
- 
-
-bool Queue_enqueue(Queue* queue, int element) {
-           if (queue->rear % queue->capacity == queue->front) 
-               {
-               return false;
-               }
-
-           else if (Queue_isEmpty) 
-                  {
-                  queue->rear = queue->front = 0;
-                  return true;
-                  }
-
- 
-
-            else 
-                  {
-                  queue->rear = queue->rear++ % queue->capacity;
-                  return true;
-                  }
-                  queue->memory[queue->rear] = element;
-
-}
-
-
-bool Queue_isEmpty(Queue* queue)
-
+bool Queue_enqueue(Queue* queue, int element)
 {
+    if (queue != NULL) {
+        if (Queue_isEmpty(queue)) {
+            queue->back = queue->front = 0;
+            queue->memory[queue->back] = element;
+            queue->back = (queue->back + 1) % queue->capacity;
+            queue->length++;
+            return true;
+        }
+        else if (Queue_isFull(queue)) {
+            return false;
+        }
+        else {
+            queue->memory[queue->back] = element;
+            queue->back = (queue->back + 1) % queue->capacity;
+            queue->length++;
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+bool Queue_dequeue(Queue* queue, int* element)
+{
+    if (queue != NULL) {
+        if (Queue_isEmpty(queue)) {
+            return false;
+        }
+        else if (queue->front == queue->back) {
+            *element = queue->memory[queue->front];
+            queue->back = queue->front = -1;
+            queue->length = 0;
+            return true;
+        }
+        else
+        {
+            *element = queue->memory[queue->front];
+            queue->front = (queue->front + 1) % queue->capacity;
+            queue->length--;
+            return true;
+        }
 
-          
-
-           int front = queue->front;
-           int rear = queue->rear;
-           if (queue->front && queue->rear == -1) {
-
-                     return true;
-
-           }
+    }
+    else {
+        return false;
+    }
 }
 
-/*
-
-size_t Queue_capacity(Queue* queue) {
-           return sizeof(queue->memory);
-
- 
-
+void Queue_clear(Queue* queue)
+{
+    if (queue != NULL) {
+        queue->front = queue->back = -1;
+        queue->length = 0;
+    }
+    else {
+        return;
+    }
 }
 
- 
-
-size_t Queue_length(Queue* queue) {
-
-           int length;
-           if (queue->front < queue->rear) {
-                     length = queue->rear - queue->front + 1;
-                     return length;
-
-           }
-
-           else if (Queue_isEmpty)
-                     return length = 0;
-           else if (queue->front == queue->rear && queue->rear != -1)
-                     return length = 1;
-
- 
-
- 
-
- 
-
-           else if (queue->front > queue->rear) {
-                     length = queue->front - queue->rear + 1;
-                     return length = queue->front - queue->rear + 1;
-
-           }
-
+bool Queue_isFull(Queue* queue)
+{
+    if (queue != NULL)
+    {
+        if ((queue->back + 1) % queue->capacity == queue->front) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+bool Queue_isEmpty(Queue* queue)
+{
+    if (queue != NULL) {
+        if (queue->front == -1 && queue->back == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 }
 
-*/
+size_t Queue_capacity(Queue* queue)
+{
+    if (queue != NULL) {
+        return queue->capacity;
+    }
+    else return NULL;
+}
+
+size_t Queue_length(Queue* queue)
+{
+    if (queue != NULL) {
+        if (Queue_isEmpty) {
+            return 0;
+        }
+        else {
+            return queue->length;
+        }
+    }
+    else return NULL;
+}
+
+bool Queue_peek(Queue* queue, int* element)
+{
+    if (queue != NULL) {
+        if (Queue_isEmpty(queue)) {
+            return false;
+        }
+        else
+        {
+            *element = queue->memory[queue->front];
+            return true;
+        }
+    }
+    else return false;
+}
 
